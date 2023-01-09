3c79ffe3e461a38e4f073a671a86e8767008028a
abre0801 <114367278+abre0801@users.noreply.github.com>
2023-01-09 10:38:24 +0000

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,31 +2,26 @@ import React, { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+//function App() {
+//const [count, setCount] = useState(0);
+<div id="App">
+<script>
+  function Header(){
+      const tabDeCourse = ["sel", "poivre", "donut"];
+      return tabDeCourse.map((tabDeCourse) => <li>{tabDeCourse}</li>);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((countParam) => countParam + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </div>
-  );
-}
+  }
+  function HomePage(){
+         <div>
+            <Header />
+         </div>
+  }
+
+
+
+</script>
+</div>
+
+
 
 export default App;
